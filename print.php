d0fbef161bf815299c35f8b4de0467b7673207e7
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-05-20 17:50:45 +0200

    Feat: $count

--- print.php
+++ print.php
@@ -1,7 +1,8 @@
 <?php 
 $user_paragraf = $_GET["userparagraf"];
 $user_censured = $_GET["usercensured"];
-$censured_paragraf = str_ireplace($user_censured, '****', $user_paragraf);
+$count = 0;
+$censured_paragraf = str_ireplace($user_censured, '****', $user_paragraf, $count);
 $censured_length = strlen($user_paragraf);
 ?>
 
@@ -15,6 +16,7 @@ $censured_length = strlen($user_paragraf);
 <body>
     <h1>Paragrafo</h1>
     <h2>Il mio paragrafo è composto da <?php echo strlen($user_paragraf);?> caratteri</h2>
+    <h3>la parola è stata censurata <?php echo $count ?> volte</h3>
     <p><?php echo $censured_paragraf  ?></p>
     
 </body>
