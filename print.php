46b4dcc19fc3ef908faa2fb8f1fbf743163832a0
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-05-20 18:00:12 +0200

    Feat: add custom style to print.php

--- print.php
+++ print.php
@@ -11,13 +11,15 @@ $censured_length = strlen($user_paragraf);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleprint.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Paragrafo</h1>
-    <h2>Il mio paragrafo è composto da <?php echo strlen($user_paragraf);?> caratteri</h2>
-    <h3>la parola è stata censurata <?php echo $count ?> volte</h3>
-    <p><?php echo $censured_paragraf  ?></p>
-    
+    <div class="container">
+        <h1>Paragrafo</h1>
+        <h2>Il mio paragrafo è composto da <?php echo strlen($user_paragraf);?> caratteri</h2>
+        <h3>la parola è stata censurata <?php echo $count ?> volte</h3>
+        <p><?php echo $censured_paragraf  ?></p>    
+    </div>
 </body>
 </html>
